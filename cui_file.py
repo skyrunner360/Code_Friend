0dbf5ced55f56f8b6396c1dacfebc20f2cdeae41
skyrunner360 <mathurrishi31@gmail.com>
2020-04-30 14:56:55 +0530

    Initial commit

---
+++ cui_file.py
@@ -0,0 +1,18 @@
+#call created functions here
+#TODO Maybe Obsolute file. Complete GUI TO Delete
+# import tkinter as tk
+import tools_func
+
+class mainclass:
+    def gui_g(self,se):
+        ex=tools_func.non_gui()
+        ex.search_google(se)
+    def gui_yt(self,se):
+        execute=tools_func.non_gui()
+        execute.search_youtube(se)
+    def gui_stack(self,se):
+        execute=tools_func.non_gui()
+        execute.search_stackoverflow(se)
+    def gui_git(self,se):
+        execute = tools_func.non_gui()
+        execute.search_github(se)
