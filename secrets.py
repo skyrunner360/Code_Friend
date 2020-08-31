5d4b722efc72102b1069e4130567f0bdb4edd344
Nikolay Chechulin <chechulin646@gmail.com>
2020-08-31 19:30:18 +0300

    update URLs to use search queries

--- secrets.py
+++ secrets.py
@@ -1,7 +1,7 @@
-GOOGLE_URL = 'https://www.google.com/'
-YOUTUBE_URL = 'https://www.youtube.com/'
-STACKOVERFLOW_URL = 'https://stackoverflow.com/'
-GITHUB_URL = 'https://github.com/'
+GOOGLE_URL = 'https://www.google.com/search?&q='
+YOUTUBE_URL = 'https://www.youtube.com/results?search_query='
+STACKOVERFLOW_URL = 'https://stackoverflow.com/search?q='
+GITHUB_URL = 'https://github.com/search?q='
 
 GITHUB_UID = ''  # Github User ID
 GITHUB_PASS = ''  # Github Password
