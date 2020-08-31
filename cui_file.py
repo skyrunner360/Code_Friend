b7c7a902849d8622e263905e29e52354d327fe5b
Nikolay Chechulin <chechulin646@gmail.com>
2020-08-31 19:43:18 +0300

    add docstring and rename NonGui to Browser

--- cui_file.py
+++ cui_file.py
@@ -5,17 +5,17 @@ import tools_func
 
 class MainClass:
     def gui_g(self, se):
-        ex = tools_func.NonGui()
+        ex = tools_func.Browser()
         ex.search_google(se)
 
     def gui_yt(self, se):
-        execute = tools_func.NonGui()
+        execute = tools_func.Browser()
         execute.search_youtube(se)
 
     def gui_stack(self, se):
-        execute = tools_func.NonGui()
+        execute = tools_func.Browser()
         execute.search_stackoverflow(se)
 
     def gui_git(self, se):
-        execute = tools_func.NonGui()
+        execute = tools_func.Browser()
         execute.search_github(se)
--- tools_func.py
+++ tools_func.py
@@ -7,27 +7,33 @@ from time import sleep
 from urllib.parse import quote
 
 
-class NonGui():
+class Browser():
+    """Operates selenium driver"""
+
     def __init__(self):
-        # Open Web Browser
         self.driver = webdriver.Firefox()
 
     def get_search_query(self, search):
+        """Return search query which can be appended to URL"""
+        """This can prevent us from searching manually"""
         return quote(search)
 
     def search_google(self, search):
-        # Open Google and search for the search string
+        """Goes to Google results page"""
         url = GOOGLE_URL + self.get_search_query(search)
         self.driver.get(url)
 
     def search_youtube(self, search):
+        """Goes to YouTube results page"""
         url = YOUTUBE_URL + self.get_search_query(search)
         self.driver.execute_script(f'window.open("{url}", "new_window")')
 
     def search_stackoverflow(self, search):
+        """Goes to StackOverflow results page"""
         url = STACKOVERFLOW_URL + self.get_search_query(search)
         self.driver.execute_script(f'window.open("{url}", "new_window")')
 
     def search_github(self, search):
+        """Goes to GitHub results page"""
         url = GITHUB_URL + self.get_search_query(search)
         self.driver.execute_script(f'window.open("{url}", "new_window")')
